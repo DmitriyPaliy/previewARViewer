5c5c71e70d59afa39362fbbc2eadd8e85fc1f809
Fusion-DmitriyPaliy <dmitriy.paliy@fusion-tech.pro>
2023-10-06 10:17:34 +0300

    add file

--- App.tsx
+++ App.tsx
@@ -1,118 +1,124 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import * as React from 'react';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Platform,
+  TouchableHighlight,
+  Text,
 } from 'react-native';
+import {ArViewerView} from 'react-native-ar-viewer';
+import RNFS from 'react-native-fs';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
+  const [localModelPath, setLocalModelPath] = React.useState<string>();
+  const [showArView, setShowArView] = React.useState(true);
+  const ref = React.useRef() as React.MutableRefObject<ArViewerView>;
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const loadPath = async () => {
+    const modelSrc =
+      Platform.OS === 'android'
+        ? 'https://github.com/riderodd/react-native-ar/blob/main/example/src/dice.glb?raw=true'
+        : 'https://github.com/riderodd/react-native-ar/blob/main/example/src/dice.usdz?raw=true';
+    const modelPath = `${RNFS.DocumentDirectoryPath}/model.${
+      Platform.OS === 'android' ? 'glb' : 'usdz'
+    }`;
+    const exists = await RNFS.exists(modelPath);
+    if (!exists) {
+      await RNFS.downloadFile({
+        fromUrl: modelSrc,
+        toFile: modelPath,
+      }).promise;
+    }
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    console.log('modelPath>>>', modelPath);
+
+    setLocalModelPath(modelPath);
+  };
+
+  React.useEffect(() => {
+    loadPath();
+  });
+
+  const takeSnapshot = () => {
+    ref.current?.takeScreenshot().then(async base64Image => {
+      const date = new Date();
+      const filePath = `${
+        RNFS.CachesDirectoryPath
+      }/arscreenshot-${date.getFullYear()}-${date.getMonth()}-${date.getDay()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}.jpg`;
+      await RNFS.writeFile(filePath, base64Image, 'base64');
+      console.log('Screenshot written to ' + filePath);
+    });
+  };
+
+  const reset = () => {
+    ref.current?.reset();
+  };
+
+  const rotate = () => {
+    ref.current?.rotate(0, 25, 0);
+  };
+
+  const mountUnMount = () => setShowArView(!showArView);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      {localModelPath && showArView && (
+        <ArViewerView
+          model={localModelPath}
+          style={styles.arView}
+          disableInstantPlacement
+          manageDepth
+          allowRotate
+          allowScale
+          allowTranslate
+          onStarted={() => console.log('started')}
+          onEnded={() => console.log('ended')}
+          onModelPlaced={() => console.log('model displayed')}
+          onModelRemoved={() => console.log('model not visible anymore')}
+          ref={ref}
+        />
+      )}
+      <View style={styles.footer}>
+        <TouchableHighlight onPress={takeSnapshot} style={styles.button}>
+          <Text>Take Snapshot</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={mountUnMount} style={styles.button}>
+          <Text>{showArView ? 'Unmount' : 'Mount'}</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={reset} style={styles.button}>
+          <Text>Reset</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={rotate} style={styles.button}>
+          <Text>Rotate</Text>
+        </TouchableHighlight>
+      </View>
     </View>
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  arView: {
+    flex: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  footer: {
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexWrap: 'nowrap',
+    flexDirection: 'row',
+    backgroundColor: 'white',
   },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    borderColor: 'black',
+    borderWidth: 1,
+    backgroundColor: 'white',
+    padding: 10,
+    margin: 5,
   },
 });
-
-export default App;
